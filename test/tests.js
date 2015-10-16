64ada66a6389c667394190df5a8af523bb01ec77
alu0100713435 <alu0100713435@ull.edu.es>
2015-10-16 11:02:50 +0100

    tteesstt

--- test/tests.js
+++ test/tests.js
@@ -14,35 +14,8 @@ describe("Test Conversor", function() {
       expect(t.celsius()).to.equal("21.2 Farenheit");
     });
 
-    it("Debería ser: 3.2e4F", function() {
+    it("Tercera: Probando la funcion calculate", function() {
       var t = new Temperatura(0,"C");
-      temp.set_valor(3.2e4);
-      temp.set_tipo("F");
-      expect(temp.get_valor()).to.equal(3.2e4);
-      expect(temp.get_tipo()).to.equal("F");
-    });
-
-    it("0.032C === 32.0576F", function() {
-      var temp = new Temperatura();
-      temp.set_valor(0.032);
-      temp.set_tipo("C");
-      var res = temp.to_f();
-      expect(res).to.equal(32.0576);
-    });
-
-    it("32,0576F === 0.032C", function() {
-      var temp = new Temperatura();
-      temp.set_valor(32.0576);
-      temp.set_tipo("F");
-      var res = temp.to_c();
-      expect(res).to.equal(0.032000000000000424);
-    });
-
-    it("5X === ERROR", function() {
-      window.onload = function() {
-        var temp = new Temperatura(5,0,"X");
-        conversor();
-        expect(fin.innerHTML).to.match("/no es correcto/");
-      }
+      expect(calculate()).to.equal(null);
     });
 });
