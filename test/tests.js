7de3e1a9a689be37cffb0e321a168d0e004c3304
alu0100713435 <alu0100713435@ull.edu.es>
2015-10-16 10:54:42 +0100

    wow

--- test/tests.js
+++ test/tests.js
@@ -10,12 +10,12 @@ describe("Test Conversor", function() {
     });
 
     it("Segunda: -6C", function() {
-      var temp = new Temperatura(-6,"C");
+      var t = new Temperatura(-6,"C");
       expect(t.celsius()).to.equal("21.2 Farenheit");
     });
 
     it("Debería ser: 3.2e4F", function() {
-      var temp = new Temperatura(0,"C");
+      var t = new Temperatura(0,"C");
       temp.set_valor(3.2e4);
       temp.set_tipo("F");
       expect(temp.get_valor()).to.equal(3.2e4);
@@ -36,14 +36,6 @@ describe("Test Conversor", function() {
       temp.set_tipo("F");
       var res = temp.to_c();
       expect(res).to.equal(0.032000000000000424);
-    });
-
-    it("mostrar_final()", function() {
-      window.onload = function() {
-        var temp = new Temperatura(5,0,"F");
-        temp.mostrar_final();
-        expect(fin.innerHTML).to.equal("El resultado es: 5 F");
-      }
     });
 
     it("5X === ERROR", function() {
