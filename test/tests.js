e8eb4a055098f511318d90cb3fadc72f1ce7c67b
alu0100713435 <alu0100713435@ull.edu.es>
2015-10-16 10:46:19 +0100

    gitignore

--- test/tests.js
+++ test/tests.js
@@ -10,7 +10,7 @@ describe("Test Conversor", function() {
       //expect(temp.get_tipo()).to.equal("F");
     });
 
-    it("Debería ser: -4.3e-2C", function() {
+    it("Segunda: -4.3e-2C", function() {
       var temp = new Temperatura(-4.3e-2,"C");
       expect(temp.get_valor()).to.equal(-4.3e-2);
       expect(temp.get_tipo()).to.equal("C");
